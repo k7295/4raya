82e42fdd88acce5dbf2af937936b86659550a4f4
k7295 <klcho.o@hotmail.com>
2018-04-07 19:15:21 -0600

    code update

--- minimax.py
+++ minimax.py
@@ -2,104 +2,100 @@
 import random
 
 class Minimax(object):
-    """ Minimax object that takes a current connect four board state
+    """ Minimax object that takes a current connect four tablero state
     """
     
-    board = None
-    colors = ["x", "o"]
+    tablero = None
+    ficha = ["x", "o"]
 
     
-    def __init__(self, board):
+    def __init__(self, tablero):
         # actualizar tablero
-        self.board = [x[:] for x in board]
+        self.tablero = [x[:] for x in tablero]
         
             
-    def bestMove(self, depth, state, curr_player):
+    def mejorMov(self, profundidad, state, jugador_actual):
         """ Returns the best move (as a column number) and the associated alpha
-            Calls search()
+            Calls buscarAlpha()
         """
         
         # para saber cuando esta moviendo X o O
-        if curr_player == self.colors[0]:
-            opp_player = self.colors[1]
+        if jugador_actual == self.ficha[0]:
+            jugador_oponente = self.ficha[1]
         else:
-            opp_player = self.colors[0]
+            jugador_oponente = self.ficha[0]
         
-        legal_moves = {} # posibles movimientos
+        movimiento_posibles = {} # posibles movimientos
         for col in range(7):
-            if self.isLegalMove(col, state):
+            if self.isMovimientoPosible(col, state):
                 # se realiza el movimiento
-                temp = self.makeMove(state, col, curr_player)
-                legal_moves[col] = -self.search(depth-1, temp, opp_player)
+                temp = self.Mover(state, col, jugador_actual)
+                movimiento_posibles[col] = -self.buscarAlpha(profundidad-1, temp, jugador_oponente)
         
         best_alpha = -99999999
-        best_move = None
-        moves = legal_moves.items()
-        print(moves)
-        random.shuffle(list(moves))
-        for move, alpha in moves:
+        mejor_mov = None
+        movimientos = movimiento_posibles.items()
+        print(movimientos)
+        random.shuffle(list(movimientos))
+        for move, alpha in movimientos:
             print(alpha)
             print(move)
             if alpha >= best_alpha:
                 best_alpha = alpha
-                best_move = move
+                mejor_mov = move
 
                 print(best_alpha)
-                print(best_move)
+                print(mejor_mov)
         
-        return best_move, best_alpha
+        return mejor_mov, best_alpha
         
-    def search(self, depth, state, curr_player):
+    def buscarAlpha(self, profundidad, state, jugador_actual):
         """ 
             Se busca en el arbol el valor del alpha,
         """
-        legal_moves = [] # posibles movimientos
+        movimiento_posibles = [] # posibles movimientos
         for i in range(7):
-            if self.isLegalMove(i, state):
+            if self.isMovimientoPosible(i, state):
                 # realiza el movimiento en la columna i del jugador actual
-                temp = self.makeMove(state, i, curr_player)
-                legal_moves.append(temp)
+                temp = self.Mover(state, i, jugador_actual)
+                movimiento_posibles.append(temp)
         
-        if depth == 0 or len(legal_moves) == 0 or self.gameIsOver(state): # profundidad 0, ultimo nodo o si ya no hay mas movimiento
+        if profundidad == 0 or len(movimiento_posibles) == 0 or self.gameOver(state): # profundidad 0, ultimo nodo o si ya no hay mas movimiento
             # retorna la heuristica 
-            return self.value(state, curr_player)
+            return self.value(state, jugador_actual)
         
         # ficha del oponente
-        if curr_player == self.colors[0]:
-            opp_player = self.colors[1]
+        if jugador_actual == self.ficha[0]:
+            jugador_oponente = self.ficha[1]
         else:
-            opp_player = self.colors[0]
+            jugador_oponente = self.ficha[0]
 
         alpha = -99999999
-        for child in legal_moves:
-            if child == None:
-                print("child == None (search)")
-            alpha = max(alpha, -self.search(depth-1, child, opp_player))
+        for nodoHijo in movimiento_posibles:
+            if nodoHijo == None:
+                print("nodoHijo == None (buscarAlpha)")
+            alpha = max(alpha, -self.buscarAlpha(profundidad-1, nodoHijo, jugador_oponente))
         return alpha
 
-    def isLegalMove(self, column, state):
+    def isMovimientoPosible(self, column, state):
         """ Funcion hecha con el fin de saber si el espacio en donde se quiere colocar una ficha
             este vacia, en caso contrario significa que esta ocupada y no se puede colocar la ficha
         """
-        
         for i in range(6):
-            if state[i][column] == ' ':
-                # once we find the first empty, we know it's a legal move
+            if state[i][column] == ' ': # espacio vacio? mueva 
                 return True
-        
-        # if we get here, the column is full
         return False
     
-    def gameIsOver(self, state):
-        if self.checkForStreak(state, self.colors[0], 4) >= 1:
+    def gameOver(self, state):
+        if self.checkRaya(state, self.ficha[0], 4) >= 1:
             return True
-        elif self.checkForStreak(state, self.colors[1], 4) >= 1:
+        elif self.checkRaya(state, self.ficha[1], 4) >= 1:
             return True
         else:
             return False
         
     
-    def makeMove(self, state, column, color):
+    def Mover(self, state, column, color):
         """ realiza un movimieto de estado en el tablero del jugador actual, 
             esta funcion me tira todos los posibles movimientos 
             
@@ -123,37 +119,37 @@ class Minimax(object):
             (num  2-in-a-rows)*10 - (num of opponent 4-in-a-rows)*99999 - (num  opponent
             3-in-a-rows)*100 - (num  opponent 2-in-a-rows)*10
         """
-        if color == self.colors[0]:
-            o_color = self.colors[1]
+        if color == self.ficha[0]:
+            o_color = self.ficha[1]
         else:
-            o_color = self.colors[0]
+            o_color = self.ficha[0]
         
-        my_fours = self.checkForStreak(state, color, 4)
-        my_threes = self.checkForStreak(state, color, 3)
-        my_twos = self.checkForStreak(state, color, 2)
-        opp_fours = self.checkForStreak(state, o_color, 4)
+        fourrow = self.checkRaya(state, color, 4)
+        threerow = self.checkRaya(state, color, 3)
+        tworow = self.checkRaya(state, color, 2)
+        fourrow_contrincante = self.checkRaya(state, o_color, 4)
     
-        if opp_fours > 0:
+        if fourrow_contrincante > 0:
             return -100000
         else:
-            return my_fours*100000 + my_threes*100 + my_twos
+            return fourrow*100000 + threerow*100 + tworow
             
-    def checkForStreak(self, state, color, streak):
+    def checkRaya(self, state, color, streak):
         count = 0
         # se recorre el tablero 
         for i in range(6):
             for j in range(7):
                 if state[i][j].lower() == color.lower():
-                    count += self.verticalStreak(i, j, state, streak)
+                    count += self.vertical_raya(i, j, state, streak)
                  
-                    count += self.horizontalStreak(i, j, state, streak)
+                    count += self.horizontal_raya(i, j, state, streak)
  
-                    count += self.diagonalCheck(i, j, state, streak)
+                    count += self.diagonal_raya(i, j, state, streak)
 
         # retorna el peso del arbol 
         return count
             
-    def verticalStreak(self, row, col, state, streak):
+    def vertical_raya(self, row, col, state, streak):
         consecutiveCount = 0
         for i in range(row, 6):
             if state[i][col].lower() == state[row][col].lower():
@@ -166,7 +162,7 @@ class Minimax(object):
         else:
             return 0
     
-    def horizontalStreak(self, row, col, state, streak):
+    def horizontal_raya(self, row, col, state, streak):
         consecutiveCount = 0
         for j in range(col, 7):
             if state[row][j].lower() == state[row][col].lower():
@@ -180,7 +176,7 @@ class Minimax(object):
             return 0
 
     
-    def diagonalCheck(self, row, col, state, streak):
+    def diagonal_raya(self, row, col, state, streak):
         
         total = 0
 
